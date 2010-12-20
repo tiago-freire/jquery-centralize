4f42f88d18a4f609e03351f31173b950147106da
freire.tiago@gmail.com <freire.tiago@gmail.com@5b0fe0e5-34d3-038a-3147-57f8fe805cc4>
2010-12-20 19:46:34 +0000

    Importação inicial

---
+++ jquery-centralize/js/jquery-centralize-0.1.js
@@ -0,0 +1,29 @@
+jQuery.fn.extend({
+	centralize: function() {
+		var children = this;
+		var totalWidth = children.parent().width();
+		
+		children.load(function() {
+			var totalCount = children.length;
+			var sumWidth = 0;
+			children.each(function() {
+				$(this).css('float', 'left');
+				sumWidth += $(this).outerWidth();
+			});
+
+			var averageWidth = sumWidth / totalCount;
+
+			var maxCountByLine = Math.floor(totalWidth / averageWidth);
+			var count = totalCount > maxCountByLine ? maxCountByLine : totalCount;
+
+			var margin = Math.floor((totalWidth - (count * averageWidth)) / (count + 1));
+
+			children.css('margin-left', margin + 'px');
+
+		}).each(function() {
+			if(this.complete || ($.browser.msie && parseInt($.browser.version) == 6)) {
+				$(this).trigger("load");
+			}
+		});
+	}
+});
